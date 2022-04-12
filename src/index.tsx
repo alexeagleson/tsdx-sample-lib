7e56aa8bfd233e1969e3db75d336e6d558adb2e3
Alex Eagleson <alexeagleson@gmail.com>
2022-04-11 23:17:55 -0400

    fix: theme export

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, HTMLAttributes, ReactChild } from 'react';
 import { Theme } from 'antd/lib/config-provider/context';
-const theme = require('theme') as Theme;
+const theme = require('./theme') as Theme;
 
 export interface Props extends HTMLAttributes<HTMLDivElement> {
   /** custom content, defaults to 'the snozzberries taste like snozzberries' */
